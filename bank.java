988cecc45b638b8bd6bedd97da963a5fdb4975fd
elhackeado <theelhackeado@gmail.com>
2017-06-15 19:34:57 +0530

    Create bank.java
    creating new file

--- bank.java
+++ bank.java
@@ -1,69 +1,49 @@
+import java.io.*;
 class bank
 {
-int accountno;
-String name;
-int balance;
- BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-void set()
-{
-System.out.print("Enter the account new number : ");
-accountno= Integer.parseInt(br.readLine());
-System.out.print("Enter the name: ");
-name=br.readLine();
-System.out.print("Enter the balance: ");
-balance= Integer.parseInt(br.readLine);
-
-}
-
-
-
-void show() throws Exception
-
-{
-System.out.print("Enter your Account number: ");
-int a= Integer.parseInt(br.readLine());
-if(a==accountno)
-{
-System.out.println("The account number is" + accountno);
-System.out.println("Name: " + name);
-System.out.println("balance");
-else
-{
-System.out.print("Invalid Account number !!");
-}
-}
-
-public static void main(String args[])
-{
-
-Bank b1= new ban();
-while(true)
-{
-System.out.println("Welcome to our banking services")
-System.out.prinln("=============================")
-System.out.println("1. SET");
-System.out.println("2. SHOW");
-System.out.println(3. EXIT);
-System.out.print("CHOOSE ANY ONE OPTION: ");
-
-
-}
-
-
-
-
-
-
-}
-
-
-
-}
-
-
-
-
-
-
+	int account;
+	String name;
+	int balance;
+	
+	BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+	
+	void set() throws Exception
+	{
+		System.out.print("Enter the account number: ");
+		account= Integer.parseInt(br.readLine());
+		System.out.print("Enter the name: ");
+		name= br.readLine();
+		System.out.print("Enter the balance: ");
+		balance= Integer.parseInt(br.readLine());
+	}
+	
+	void show() throws Exception
+	{
+		
+		System.out.println("Account Number: "+account);
+		System.out.println("Name: " + name);
+		System.out.println("Balance: " + balance);
+		
+	}
+	
+	
+	public static void main(String args[]) throws Exception
+	{
+		bank a= new bank();
+		bank b[] = new bank[3];
+		int i;
+		for(i=0;i<b.length;i++)
+		{
+			b[i]= new bank();
+			b[i].set();
+		}
+		    
+		System.out.print("Enter any number::");
+		int inp= Integer.parseInt(a.br.readLine());
+		i=inp;
+		b[i].show();
+	
+	
+	}
+	
 }
