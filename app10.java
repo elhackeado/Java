0ab1a5187e3ac5ff82e6a86516a7a49e4a65fc97
El Hackeado <theelhackeado@gmail.com>
2017-06-23 23:19:01 +0530

    MOVING STRINGS ONE BY ONE

---
+++ app10.java
@@ -0,0 +1,63 @@
+/*
+<applet code="app10" height="400" width="400">
+</applet>
+*/
+import java.applet.*;
+import java.awt.*;
+
+public class app10 extends Applet
+{
+	String msg1="HELLO";
+	String msg2="HII";
+	String msg3="WELCOME";
+	int x1=10;
+	int x2=10;
+	int x3=10;
+	int y=150;
+	int flag=0;
+	public void paint(Graphics g)
+	{
+		
+		g.drawString(msg1,x1,y);
+		g.drawString(msg2,x2,y+30);
+		g.drawString(msg3,x3,y+60);
+		
+		
+		if(flag==0&&x1==350)
+		{
+			flag=1;
+		}
+		else if(flag==1&&x2==350)
+		{
+			flag=2;
+		}
+		else if(flag==2&x3==350)
+		{}
+		else if(flag==0)
+		{
+			x1++;
+		}
+		else if(flag==1)
+		{
+			x2++;
+		}
+		else if(flag==2)
+		{
+			x3++;
+		}
+		try
+		{
+			Thread.sleep(50);
+		}
+		catch(Exception e)
+		{
+			
+		}
+
+		repaint();	
+		
+		
+		
+	}
+	
+}
